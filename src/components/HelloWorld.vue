5601ef2c55cb3050f9a7b12ba30955e921adcc0a
朱俊杰 <jekingzhu@jmtop.com>
2021-10-20 18:14:36 +0800

    add sheet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,54 @@
 <template>
-  <div class="hello">
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-	<alert></alert>
-  </div>
+	<div class="hello">
+		<div @click="showAlert">Alert</div>
+		<div v-if="isShowAlert"><alert @close="close"></alert></div>
+		
+	</div>
 </template>
 
 <script>
 	import alert from '../packages/jj-alert.vue'
-export default {
-  name: 'HelloWorld',
-  components:{alert},
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		components: {
+			alert
+		},
+		props: {
+			msg: String
+		},
+		data() {
+			return {
+				isShowAlert: false,
+			}
+		},
+		methods: {
+			showAlert() {
+				this.isShowAlert = true
+			},
+			close() {
+				this.isShowAlert = false
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	h3 {
+		margin: 40px 0 0;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
 </style>
--- src/packages/jj-alert.vue
+++ src/packages/jj-alert.vue
@@ -1,14 +1,23 @@
 <template>
 	<div class="shade" @click="isTouchClose?close():''">
-		<div class="main">
+		<div class="main" :style="{'width':contentWidth}">
 			<div class="content">
 				<div v-if="isShowClose" class="rightTopClose" @click="close">x</div>
 				<div v-if="title.length > 0" class="flexCenter" :style="titleStyle"><span>{{title}}</span></div>
 				<div v-if="message.length > 0" class="flexCenter" style="margin-top: 10px;" :style="messageStyle"><span>{{message}}</span></div>
 			</div>
-			<div class="btns">
-				<div class="btn" v-for="(btn,index) in btns" :style="btnStyle(btn)" :key="index"
-					@click="clickFn($event,btn)">{{btn.title}}
+			<div v-if="type === 'alert'">
+				<div class="btns flexContentSpaceAround">
+					<div class="btn" v-for="(btn,index) in btns" :style="btnStyle(btn)" :key="index"
+						@click="clickFn($event,btn)">{{btn.title}}
+					</div>
+				</div>
+			</div>
+			<div v-else>
+				<div class="btns flexContentCenter" v-for="(btn,index) in btns" :key="index">
+					<div class="btn" :style="btnStyle(btn)" 
+						@click="clickFn($event,btn)">{{btn.title}}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -19,54 +28,49 @@
 	export default {
 		data() {
 			return {
+				type:'alert', //有alert和sheet
 				isTouchClose: false, //点击背景图层，是否关闭弹框
 				isShowClose:false,//是否显示右上角的关闭按钮
-				title:"提示提示",
+				mainStyle:{'width':'300px'},
+				contentWidth:'80%',//内容显示框的大小，可以按照窗口的百分比指定大小，也可以是具体px,如300px
+				title:"提示",
 				titleStyle:{},
 				// titleStyle:{"justify-content":'left',"display": "flex","color":'#fe2','text-align':'left'},
-				message:"",
+				message:"fjeojfoefjeofjeo",
 				messageStyle:{},
 				btns: [{
-						title: "取消取消取消",
-						style: {
-							"font-size": '12px'
-						},
-						isTouchClose: true, //点击按钮，是否触摸后进行关闭弹框
-						click: () => {
-							console.log("取消取消取消")
-						},
-					},
-					{
 						title: "确认",
 						style: {
 							"color": '#fff',
 							'background': '#2A8AFF'
 						},
+						isTouchClose: true, //点击按钮，是否自动关闭弹框
 						click: () => {
-							console.log("确认")
-						}
+							console.log("按钮被点击")
+						},
 					},
-					{
-						title: "确认",
-						click: () => {
-							console.log("确认")
-						}
-					}
 				]
 			}
 		},
+		computed:{
+		},
 		methods: {
 			close() {
+				this.$emit('close')
 				this.$el.remove()
 			},
 			btnStyle(btn) {
 				let length = this.btns.length
-
 				let style = {}
 				style['width'] = 100 / length + '%'
 				if (btn.style instanceof Object) {
 					//样式的合并
 					Object.assign(style, btn.style)
+				}
+				if (this.type === 'sheet'){
+					style['flex-direction'] = 'column'
+					style['width'] = '100%'
+					style['border-right'] = '0px'
 				}
 				return style
 			},
@@ -105,21 +109,30 @@
 		right: 0px;
 	}
 
-	.btns {
-		height: 45px;
-		box-sizing: border-box;
-		border-top: 1px solid rgba(0, 0, 0, 0.1);
+    .flexContentCenter{
 		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		
+	}
+	.flexContentSpaceAround{
+		display: flex;
+		flex-direction: row;
 		justify-content: space-around;
 		align-items: center;
-
+	}
+	.btns {
+		
+		box-sizing: border-box;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
 	}
 
 	.btn {
 		flex: 1 0 auto;
-		height: 100%;
+		height: 2.75rem;
 		display: flex;
-		font-size: 17px;
+		font-size: 1.0625rem;
 		align-items: center;
 		justify-content: center;
 		text-align: center;
@@ -132,6 +145,6 @@
 
 	.btn:active {
 		color: white;
-		background-color: blue;
+		background-color: #2A8AFF;
 	}
 </style>
